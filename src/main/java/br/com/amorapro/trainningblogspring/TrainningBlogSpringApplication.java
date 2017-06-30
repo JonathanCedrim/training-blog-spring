cfcbf8360667fbe0b19893ba15a2f0b9c919e44e
Jonathan <jonathan.cedrim.souza@gmail.com>
2017-06-30 16:49:28 -0300

    build

---
+++ src/main/java/br/com/amorapro/trainningblogspring/TrainningBlogSpringApplication.java
@@ -0,0 +1,12 @@
+package br.com.amorapro.trainningblogspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainningBlogSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainningBlogSpringApplication.class, args);
+	}
+}
